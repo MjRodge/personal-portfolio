415e16ab686f509af71181c1b85e7185f619a973
Michael Rodgers <mail@mjrodge.me>
2018-11-01 13:58:50 -0400

    Added Flex and Responsive Images to Projects

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import Img from 'gatsby-image'
 
+import './styles/projects.css'
+
 class Projects extends React.Component {
   render() {
     const projects = this.props.projects
     return (
-      <div>
+      <div id="projects-container">
         {projects.map(({ node }, i) => (
           <div className="project" key={i}>
-            <Img fixed={node.image.fixed} />
-            <h2>{node.name}</h2>
-            <p>{node.description}</p>
-            <a href={node.githubUrl}>Source Code</a>
-            <a href={node.liveUrl}>Project Demo</a>
-            {node.tech.map(tech => (
-              <p key={tech}>{tech}</p>
-            ))}
+            <Img fluid={node.image.fluid} className="project-image" />
+            <div className="project-info">
+              <h2>{node.name}</h2>
+              {node.tech.map(tech => (
+                <p key={tech}>{tech}</p>
+              ))}
+              <p>{node.description}</p>
+              <a href={node.githubUrl}>Source Code</a>
+              <a href={node.liveUrl}>Project Demo</a>
+            </div>
           </div>
         ))}
       </div>
