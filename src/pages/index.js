b366b3c481d598a9780728ae18cdd2d55aac3658
Michael Rodgers <mail@mjrodge.me>
2018-11-28 15:01:22 -0500

    Styling for hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import { Link, graphql } from 'gatsby'
 import get from 'lodash/get'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import Projects from '../components/projects'
+import Hero from '../components/hero'
 
 class IndexPage extends React.Component {
   render() {
@@ -13,13 +13,8 @@ class IndexPage extends React.Component {
 
     return (
       <Layout>
+        <Hero />
         <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-          <Image />
-        </div>
-        <Link to="/page-2/">Go to page 2</Link>
         <Projects projects={projects} />
       </Layout>
     )
